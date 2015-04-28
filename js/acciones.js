11a2cecee72abde53c91130f0861c1d2e57d6c34
AL26F13 <fermonmtz@gmail.com>
2015-04-28 13:19:35 -0500

    Dos
    Agregue las acciones de barrer y la pagina nueva

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,18 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+	$('#btnvibrar').on('tap',function (){
+		navigator.vibrate(1000);
+	}); // tap a btnvibrar
+	$('#btnbeep').on('tap', function (){
+		navigator.beep(1);
+	}); // tap a bntbeep
+	$('#izquierda').on('swipeleft', function (){
+		alert("Barrio a la izquierda")
+	});//barrer a la izquierda
+	$('#derecha').on('swiperigth', function (){
+		alert("Barrio a la derecha")
+	});//barrer a la derecha
 }); 
 });
 
