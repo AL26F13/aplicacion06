c13034e232cd798d2aec87c7a870df489a5e37b2
fer montaño <fermonmtz@gmail.com>
2015-04-29 13:27:00 -0500

    Cuatro
    notificate

--- js/acciones.js
+++ js/acciones.js
@@ -5,7 +5,7 @@ document.addEventListener("deviceready",function(){
 		navigator.vibrate(1000);
 	}); // tap a btnvibrar
 	$('#btnbeep').on('tap', function (){
-		navigator.beep(1);
+		navigator.notification.beep(1);
 	}); // tap a bntbeep
 	$('#izquierda').on('swipeleft', function (){
 		alert("Barrio a la izquierda")
