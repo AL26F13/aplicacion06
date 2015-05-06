ca15dbd1dd19dbb7ac3f4fe7d177debbc6cfdd72
fer montaño <fermonmtz@gmail.com>
2015-05-06 13:49:39 -0500

    Siete
    Agregue la pagina de listado de eventos y agregue cosas en acciones

--- js/acciones.js
+++ js/acciones.js
@@ -4,15 +4,24 @@ document.addEventListener("deviceready",function(){
 	$('#btnvibrar').on('tap',function (){
 		navigator.vibrate(1000);
 	}); // tap a btnvibrar
-	$('#btnbeep').on('tap', function (){
+	$('#btnbeep').on('tap',function (){
 		navigator.notification.beep(1);
 	}); // tap a bntbeep
-	$('#izquierda').on('swipeleft', function (){
+	$('#izquierda').on('swipeleft',function (){
 		alert("Barrio a la izquierda")
 	});//barrer a la izquierda
-	$('#derecha').on('swiperight', function (){
+	$('#derecha').on('swiperight',function (){
 		alert("Barrio a la derecha")
 	});//barrer a la derecha
+	document.addEventListener("pause",function (){
+		$('#listado').append("<p> La aplicacion se pausó </p>");
+	});//pausa cierra
+	document.addEventListener("resume",function (){
+		$('#listado').append("<p> Se reinicio la aplicacion </p>");
+	});//se despauso
+	$(window).on('orientationchange',function(e){
+		$('#listado').append("<p> Orientcion: " + e.orientation + "</p>");
+	});//cambiar de orientacion
 }); 
 });
 
